ae165809bc8c66561000d3114e90d2529958406e
stefanos <sagelastos@gmail.com>
2019-09-06 21:04:08 +0200

    working on testing routing

--- client/src/utils/route.protected.test.js
+++ client/src/utils/route.protected.test.js
@@ -1,13 +1,11 @@
+import React from 'react'
 import ProtectedRoute from "./route.protected";
 import renderer from "react-test-renderer";
 import fakeAuth from "./auth.service";
 
 jest.mock("./auth.service");
 
-test("should return A", () => {
-  fakeAuth.isAuthenticated.mockResolvedValue(true);
-  it("renders correctly", () => {
-    const tree = renderer.create(<ProtectedRoute />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+test("should return Redirect when Auth is false", () => {
+  const tree = renderer.create(<ProtectedRoute />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
